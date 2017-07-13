15552360cda20af5892969fc0314c307e23169a6
Marlon Bier <marlonbier@gmail.com>
2017-07-13 09:53:30 -0300

    Removido configuração de porta

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"os"
-
 	"github.com/go-macaron/gzip"
 	macaron "gopkg.in/macaron.v1"
-
-	"strconv"
 
 	"github.com/mbier/aceleratomo/handlers"
 )
@@ -20,16 +16,5 @@ func main() {
 	m.Get("/quadro/geral", handlers.QuadroGeral)
 	m.Get("/quadro/:nome", handlers.Quadro)
 
-	var port int
-	portString := os.Getenv("PORT")
-
-	if portString != "" {
-		var err error
-		port, err = strconv.Atoi(portString)
-		panic(err)
-	} else {
-		port = 6969
-	}
-
-	m.Run(port)
+	m.Run()
 }
